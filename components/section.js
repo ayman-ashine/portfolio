24649cbbbca642e409a4ebb6c6953a747b9c6533
Catraco <gpx.zero.000@gmail.com>
2023-07-20 22:10:37 +0100

    Update 123011

--- components/section.js
+++ components/section.js
@@ -2,7 +2,7 @@ export const Section = ({ children }) => {
 
     return (
       <div className='h-screen w-full flex items-center'>
-        <div className="container relative bg-gradient-to-t from-blue-900 border-b-4 border-blue-950 p-10 rounded-lg">
+        <div className="container relative bg-gradient-to-t from-blue-950 to-slate-950 border-b-4 border-blue-950 p-10 rounded-lg">
           {children}
         </div>
       </div>
