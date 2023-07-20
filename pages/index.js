290f6a3e5ee61d75dbcdb00d87a68205ce18f10f
Catraco <gpx.zero.000@gmail.com>
2023-07-20 17:12:02 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -205,8 +205,10 @@ export default function Home() {
 const Section = ({ children }) => {
 
   return (
-    <div className="container relative bg-gradient-to-t from-blue-900 border-b-4 border-blue-950 p-10 rounded-lg">
-      {children}
+    <div className='h-screen w-full flex items-center'>
+      <div className="container relative bg-gradient-to-t from-blue-900 border-b-4 border-blue-950 p-10 rounded-lg">
+        {children}
+      </div>
     </div>
   )
 
