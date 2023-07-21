87de070b4449a0f10548195339bd1006c8c15ba8
Catraco <gpx.zero.000@gmail.com>
2023-07-21 23:49:46 +0100

    Config contact section

--- components/section.js
+++ components/section.js
@@ -1,8 +1,8 @@
 export const Section = ({ children }) => {
 
     return (
-      <div className='h-screen w-full flex items-center'>
-        <div className="container relative bg-gradient-to-t from-blue-950 to-slate-950 border-b-4 border-blue-950 p-10 rounded-lg">
+      <div className='w-full flex items-center'>
+        <div className="container relative bg-gradient-to-t from-blue-950 border-b-4 border-blue-900 px-2 py-10 md:p-10 rounded-md">
           {children}
         </div>
       </div>
